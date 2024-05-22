1172eeee0b8aba132bc28c5944b39c1ad72c2c86
Tarek Omar <tarek.omar@hotmail.com>
2024-05-22 20:00:07 +0300

    fix lustre fs client, and fix the rank for MPI

--- src/mountlustre.sh
+++ src/mountlustre.sh
@@ -1,11 +1,16 @@
-# Execute a bash script stored in the file 'repo.bash'  
-bash repo.bash 
-  
-# Install a Azure lustre client - amlfs-lustre-client  
-apt install filesystem-client-version=$(uname -r) -y  
-  
-# Create a directory with a specific path  , change lustrefs accordingly
-mkdir -p /eph/nvme0/[lusterfs]
-  
-# Mount a filesystem of type 'lustre' at a specific location with specific options  [IP] your amlfs IP, and replace [lustrefs] accordingly
-mount -t lustre -o noatime,user_xattr [IP]@tcp0:/lustrefs /eph/nvme0/[lusterfs]
+if [[ "$LOCAL_RANK" -eq 0 ]]; then
+
+    # Execute a bash script stored in the file 'repo.bash'  
+    bash repo.bash 
+    
+    # Install a Azure lustre client - amlfs-lustre-client  
+    apt install amlfs-lustre-client-2.15.4-42-gd6d405d=$(uname -r) -y
+    
+    # Create a directory with a specific path  , change lustrefs accordingly
+    mkdir -p /eph/nvme0/[lusterfs]
+    
+    # Mount a filesystem of type 'lustre' at a specific location with specific options  [IP] your amlfs IP, and replace [lustrefs] accordingly
+    mount -t lustre -o noatime,user_xattr [IP]@tcp0:/lustrefs /eph/nvme0/[lusterfs]
+else
+    sleep 60
+fi
